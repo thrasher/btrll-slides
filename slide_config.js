e4541e91b960209b9da1989ca868ca41f3ff6fc3
Jason Thrasher <jasonthrasher@gmail.com>
2012-07-13 00:47:46 -0700

    BrightRoll Engineering branded HTML5 slides.

--- slide_config.js
+++ slide_config.js
@@ -8,7 +8,7 @@ var SLIDE_CONFIG = {
     enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
     enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
     //analytics: 'UA-XXXXXXXX-1', // TODO: Using this breaks GA for some reason (probably requirejs). Update your tracking code in template.html instead.
-    favIcon: 'images/google_developers_logo_tiny.png',
+    favIcon: 'images/btrll_circle_red_24x24.png',
     fonts: [
       'Open Sans:regular,semibold,italic,italicsemibold',
       'Inconsolata'
@@ -18,15 +18,15 @@ var SLIDE_CONFIG = {
 
   // Author information
   presenters: [{
-    name: 'Firstname Lastname',
-    company: 'Job Title, Google',
-    gplus: 'http://plus.google.com/1234567890',
-    twitter: '@yourhandle',
-    www: 'http://www.you.com',
-    github: 'http://github.com/you'
+    name: 'First Name',
+    company: 'Job Title, BrightRoll',
+    gplus: 'http://plus.google.com/101906387164396459023',
+    twitter: '@thrasher',
+    www: 'http://kyr.brightroll.com/users/138',
+    github: 'http://github.com/thrasher'
   }/*, {
     name: 'Second Name',
-    company: 'Job Title, Google',
+    company: 'Job Title, BrightRoll',
     gplus: 'http://plus.google.com/1234567890',
     twitter: '@yourhandle',
     www: 'http://www.you.com',
